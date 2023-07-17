a37fd861d9bc783135097fb236f2a261f4d6abee
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-07-17 22:19:56 +0200

    feat:final operators

--- examples/03_operators.js
+++ examples/03_operators.js
@@ -21,3 +21,46 @@ console.log("Calle Mayor" + ", 1D");
 x = x + 3;
 x += 3;
 x++;
+
+// Operadores de comparación
+
+let a = 2;
+let b = 5;
+
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+
+// === vs ==
+
+console.log("1" == 1);
+console.log("1" === 1);
+
+console.log(true == 1);
+
+// operadores lógicos
+
+let w = 5;
+let z = 53;
+
+// || or
+// && and
+
+/* Determinar si la multiplicación de estos números es igual al valorFinal?
+
+const numero1 = 20;
+const numero2 = 5; 
+const valorFinal = 120;
+
+// Usar operador aritméticos
+
+// Usar operador lógicos
+
+// mostrar con console.log*/
+
+const numero1 = 20;
+const numero2 = 5;
+const valorFinal = 120;
+console.log(numero1 * numero2 === valorFinal);
