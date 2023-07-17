43cade3251eb84e3230fc0552e8d1c17eeee1975
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-07-17 21:44:18 +0200

    feat: add operators

--- examples/01_basic.js
+++ examples/01_basic.js
@@ -1,1 +1,19 @@
 // comentarios trala la
+/*
+comentario multilinea
+
+*/
+// variables
+var edad = 30;
+var name = "Keepcoding";
+
+console.log(edad, name);
+console.log(name);
+let isAdult = true;
+console.log(isAdult);
+
+/* Define 4 variables: nombre, número favorito, color favorito. Todo con cammelCase*/
+
+let name = "juanma";
+let favNumber = 5;
+let favColor = "#ccc";
---
+++ examples/03_operators.js
@@ -0,0 +1,23 @@
+// operador de asignacion
+
+let x = 5;
+let y = 3;
+
+// aritmeticos
+
+let suma = x + y;
+let resta = x - y;
+let producto = x * y;
+let division = x / y;
+let modulo = x % y;
+let potencia = x ** y;
+console.log(suma);
+console.log(potencia);
+
+console.log("Calle Mayor" + ", 1D");
+
+// asignacion de adición
+
+x = x + 3;
+x += 3;
+x++;
