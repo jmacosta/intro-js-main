0ab2bd420f0500e6b444f03c1dfbbcef772a0070
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-07-20 22:09:39 +0200

    feat :array of arrays

--- examples/array_de_arrays.js
+++ examples/array_de_arrays.js
@@ -1,2 +1,35 @@
-const matrix = [[1, 2, 3], 2, true, 4, "pepe"];
-console.log(matrix);
+/*const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+//console.log(matrix);
+for (let index = 0; index < matrix.length; index++) {
+  for (let j = 0; j < matrix[index].length; j++) {
+    const element = matrix[index][j];
+    console.log(element);
+  }
+}
+
+// Crear un array bidimensional con 5 columnas y con 3 filas como las que aparecen abajo
+/*
+--*--
+-***-
+*****
+
+Luego de definirlo recorrerlo para que se pinte como en el dibujo
+*/
+
+const tree = [
+  ["-", "-", "*", "-", "-"],
+  ["-", "*", "*", "*", "-"],
+  ["*", "*", "*", "*", "*"],
+];
+
+for (let i = 0; i < tree.length; i++) {
+  let branch = "";
+  for (let j = 0; j < tree[i].length; j++) {
+    branch += tree[i][j];
+  }
+  console.log(branch);
+}
