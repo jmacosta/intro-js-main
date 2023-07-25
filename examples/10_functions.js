14af39f22dccd3d01c1a53286c187c1da0657ff5
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-07-25 19:58:17 +0200

    feat: functions examples

--- examples/10_functions.js
+++ examples/10_functions.js
@@ -70,16 +70,63 @@ console.log(restar(4, -2));
 const counter = () => {
   let count = 0;
   const increment = () => {
+    console.log("Le sumo uno al contador");
     count++;
   };
   getCount = () => count;
   const decrement = () => {
+    console.log("Le resto uno al contador");
     count--;
   };
-  return { increment, count: getCount };
+  return { increment, count: getCount, decrement };
 };
 
 const contador1 = counter();
 console.log(contador1.count());
 contador1.increment();
+contador1.increment();
+contador1.increment();
+contador1.increment();
 console.log(contador1.count());
+contador1.decrement();
+console.log(contador1.count());
+
+//// exercise
+
+/*
+Crear un closure de una calculadora que pueda hacer las siguientes operaciones guardando el resultado
+*/
+
+const calculadora = () => {
+  let result = 0;
+  const sumar = (value) => (result += value);
+  const restar = (value) => (result -= value);
+  const multiplicar = (value) => (result *= value);
+  const dividir = (value) => (result /= value);
+
+  return { sumar, restar, multiplicar, dividir };
+};
+
+const miCalculadora = calculadora();
+
+console.log(miCalculadora.sumar(5)); // Debería imprimir 5
+console.log(miCalculadora.restar(2)); // Debería imprimir 3
+console.log(miCalculadora.multiplicar(4)); // Debería imprimir 12
+console.log(miCalculadora.dividir(2)); // Debería imprimir 6
+console.log(miCalculadora.sumar(10)); // Debería imprimir 16
+
+// pasar una funcion como parametro
+
+const realizarOperacion = (num1, num2, operacion) => operacion(num1, num2);
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
+let resultadoSuma = realizarOperacion(1, 2, suma);
+let resultadoResta = realizarOperacion(1, 2, resta);
+let resultadoMensaje = realizarOperacion(
+  10,
+  20,
+  (num1, num2) => `${num1} - ${num2}`
+); // 10 -  20
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMensaje);
